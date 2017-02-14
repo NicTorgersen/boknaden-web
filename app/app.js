34f8858f3dae5e3e60823e8da7261735b589cf73
Nic <fristurt@hotmail.com>
2017-02-14 15:30:17 +0100

    latest changes

--- app/app.js
+++ app/app.js
@@ -1,39 +1,16 @@
-/**
- *
- * AngularJS Boilerplate
- * @description           Description
- * @author                Jozef Butko // www.jozefbutko.com/resume
- * @url                   www.jozefbutko.com
- * @version               1.1.7
- * @date                  March 2015
- * @license               MIT
- *
- */
 ;(function() {
 
-
-  /**
-   * Definition of the main app module and its dependencies
-   */
   angular
     .module('boknaden', [
       'ngRoute',
       'ngAnimate',
       'angular-storage',
+      'ui.bootstrap',
     ])
     .config(config);
 
-  // safe dependency injection
-  // this prevents minification issues
   config.$inject = ['$routeProvider', '$locationProvider', '$httpProvider', '$compileProvider'];
 
-  /**
-   * App routing
-   *
-   * You can leave it here in the config section or take it out
-   * into separate file
-   *
-   */
   function config($routeProvider, $locationProvider, $httpProvider, $compileProvider) {
 
     $locationProvider.html5Mode(false);
@@ -84,10 +61,6 @@
     };
   }
 
-
-  /**
-   * Run block
-   */
   angular
     .module('boknaden')
     .run(run);
@@ -96,7 +69,7 @@
 
   function run($rootScope, $location) {
 
-    // put here everything that you need to run on page load
+    // run on page load
 
   }
 
